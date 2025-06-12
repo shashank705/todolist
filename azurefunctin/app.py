62d2bdb139ab79c41aa30ff6630f5de3f43ed125
shashank705 <shashank9036323729@gmail.com>
2025-06-12 21:09:23 +0530

    Add files via upload

---
+++ azurefunctin/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+todo_list = []  # In-memory list
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        todo_list.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    if 0 <= task_id < len(todo_list):
+        todo_list.pop(task_id)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
